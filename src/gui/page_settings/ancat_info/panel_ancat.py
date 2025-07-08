950650fe6c0572ed4af22b9ba88ae3da7f66f01f
LC_Linkous <L.C.Linkous@gmail.com>
2025-07-07 21:39:35 -0400

    hints update

--- src/gui/page_settings/ancat_info/panel_ancat.py
+++ src/gui/page_settings/ancat_info/panel_ancat.py
@@ -29,6 +29,10 @@ class AnCATNotebookPage(wx.Panel):
         self.commentTxt = wx.StaticText(commentScroll, style=wx.TE_MULTILINE|wx.TE_RICH, size=(450, -1))
         self.commentTxt.Wrap(450)
         self.commentTxt.SetBackgroundColour((235,235,235))
+        font = wx.Font(11, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+
+        # Apply the new font to the StaticText
+        self.commentTxt.SetFont(font)
 
         # comments
         commentSizer = wx.BoxSizer(wx.VERTICAL)
@@ -46,14 +50,21 @@ class AnCATNotebookPage(wx.Panel):
         self.setCommentText()
     
     def setCommentText(self):
-        s = "\nThe Antenna Calculation and Autotuning Tool (AntennaCAT) software suite is a comprehensive " \
+        s = "\n\t The Antenna Calculation and Autotuning Tool (AntennaCAT) software suite is a comprehensive " \
         "implementation of machine learning to automate, evaluate, and optimize the antenna design " \
-        "process using EM simulation software. It utilizes a combined antenna designer pre-loaded with" \
-        " replication studies and internal calculator to accelerate the CAD construction and EM simulation " \
-        "of several common topologies, while eliminating model disparity for automated data collection." \
-        " In particular, this work includes the capability to create and export structured datasets from " \
+        "process using EM simulation software. It utilizes a combined antenna designer pre-loaded with " \
+        "replication studies and internal calculator to accelerate the CAD construction and EM simulation " \
+        "of several common topologies, while eliminating model disparity for automated data collection. " \
+        "In particular, this work includes the capability to create and export structured datasets from " \
         "the aforementioned EM software for iterative improvement and includes an expandable selection " \
         "of optimizers. AntennaCAT is designed with three things in mind: accessibility, adaptability, and experimentation.\n" \
+        "\n\t To use AntennaCAT, start by selecting an EM Simulation software on the left side of this page. " \
+        "This will update the Design and Simulation options. Next, open the Design page using the vertical button menu on the left. " \
+        "After Design as been configured, move on to the Simulation page to set simulation settings and exported reports. " \
+        "The Batch page can be used to run a parameter sweep and export data over a range of configurations. The Optimizer " \
+        "page has simplified Simulation output to allow AntennaCAT control over the report data. This Settings page can be used " \
+        "to personalize and configure the UI behavior." \
+        "\n\n\t Detailed AntennaCAT documentation can be found online at the official Wiki: https://github.com/LC-Linkous/AntennaCalculationAutotuningTool/wiki"
 
 
         self.commentTxt.SetLabel(s)
