31dcdfb1bd25fbeab67497f3435ebed927b5cd01
LC_Linkous <L.C.Linkous@gmail.com>
2025-07-07 21:03:01 -0400

    main uses config CONSTANT for versioning

--- src/main.py
+++ src/main.py
@@ -5,15 +5,16 @@
 #       This is the default entry point for the program
 #    
 #   Author: Lauren Linkous (LINKOUSLC@vcu.edu)
-#   Last update: March 10, 2025
+#   Last update: July 5, 2025
 ##--------------------------------------------------------------------\
 
 from gui.antennacat_gui import AntennaCATGUI
-
+from project.config.antennaCAT_config import ANTENNACAT_VERSION
 
 def main():
-
-       anCATgui = AntennaCATGUI(None, title='AntennaCAT v2025.1')
+       
+       t = "AntennaCAT " + str(ANTENNACAT_VERSION)
+       anCATgui = AntennaCATGUI(None, title=t)
  
 if __name__ == '__main__':
     main()
