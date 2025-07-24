ce18d18803187df5707d39a97fc185086689d706
LC_Linkous <L.C.Linkous@gmail.com>
2025-07-24 13:05:50 -0400

    numeric precision pass thru

--- src/optimizers/BAYESIAN/controller_BAYESIAN.py
+++ src/optimizers/BAYESIAN/controller_BAYESIAN.py
@@ -97,7 +97,7 @@ class CONTROLLER_BAYESIAN():
         XI = float(optimizerParams['xi'][0])   
         NUM_RESTARTS = int(optimizerParams['num_restarts'][0])    # number of restarts to minimize for randoms
 
-
+        DECIMALS = int(optimizerParams['numerical_precision'][0])
         THRESHOLD = list(optimizerParams['target_threshold'][0])          # Threshold symbols ["=", "≤", "≥"]
 
         self.useSurrogateModel = optimizerParams['use_surrogate_bool'][0]   # UPDATE THIS IN THE UI. The model is set up from an external call, (where the swarm obj is created)
@@ -162,7 +162,8 @@ class CONTROLLER_BAYESIAN():
                                 evaluate_threshold=self.evaluate_threshold, 
                                 obj_threshold=self.THRESHOLD, 
                                 useSurrogateModel=self.useSurrogateModel, 
-                                surrogateOptimizer=self.sm_opt)    
+                                surrogateOptimizer=self.sm_opt,
+                                decimal_limit=DECIMALS)    
     
         msg = "optimizer configured"
         self.debug_message_printout(msg)
@@ -203,30 +204,30 @@ class CONTROLLER_BAYESIAN():
     def get_optimized_outs(self):
         return self.optimizer.get_optimized_outs()
     
-######################################################
-# SURROGATE MODEL APPROXIMATOR FUNCS
-######################################################
+# ######################################################
+# # SURROGATE MODEL APPROXIMATOR FUNCS
+# ######################################################
 
-    def fit_model(self, x, y):
-        # call out to parent class to use surrogate model
-        self.sm_approx.fit(x,y)
+#     def fit_model(self, x, y):
+#         # call out to parent class to use surrogate model
+#         self.sm_approx.fit(x,y)
         
 
-    def model_predict(self, x, output_size=None):
-        # call out to parent class to use surrogate model
-        #'mean' is regressive definition. not statistical
-        #'variance' only applies for some surrogate models
+#     def model_predict(self, x, output_size=None):
+#         # call out to parent class to use surrogate model
+#         #'mean' is regressive definition. not statistical
+#         #'variance' only applies for some surrogate models
 
-        if output_size == None:
-            output_size = self.out_vars
+#         if output_size == None:
+#             output_size = self.out_vars
 
-        mean, noErrors = self.sm_approx.predict(x, output_size)
-        return mean, noErrors
+#         mean, noErrors = self.sm_approx.predict(x, output_size)
+#         return mean, noErrors
 
 
-    def model_get_variance(self):
-        variance = self.sm_approx.calculate_variance()
-        return variance
+#     def model_get_variance(self):
+#         variance = self.sm_approx.calculate_variance()
+#         return variance
 
  
 
