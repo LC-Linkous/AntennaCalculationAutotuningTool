5120c9bbd4b4f0c875b1b6a9e6de9da41a6fc1f8
LC_Linkous <L.C.Linkous@gmail.com>
2025-07-24 13:06:30 -0400

    numeric precision pass thru

--- src/optimizers/PSO_QUANTUM/controller_PSO_QUANTUM.py
+++ src/optimizers/PSO_QUANTUM/controller_PSO_QUANTUM.py
@@ -82,6 +82,7 @@ class CONTROLLER_PSO_QUANTUM():
                                                                           #    between the personal and global best positions
                                                                      
 
+        DECIMALS = int(optimizerParams['numerical_precision'][0])
         THRESHOLD = list(optimizerParams['target_threshold'][0])          # Threshold symbols ["=", "≤", "≥"]
 
         self.useSurrogateModel = optimizerParams['use_surrogate_bool'][0]   # UPDATE THIS IN THE UI. The model is set up from an external call, (where the swarm obj is created)
@@ -152,7 +153,8 @@ class CONTROLLER_PSO_QUANTUM():
                                 evaluate_threshold=self.evaluate_threshold, 
                                 obj_threshold=self.THRESHOLD, 
                                 useSurrogateModel=self.useSurrogateModel, 
-                                surrogateOptimizer=self.sm_opt)    
+                                surrogateOptimizer=self.sm_opt,
+                                decimal_limit=DECIMALS)        
     
         msg = "optimizer configured"
         self.debug_message_printout(msg)
@@ -193,30 +195,30 @@ class CONTROLLER_PSO_QUANTUM():
     def get_optimized_outs(self):
         return self.optimizer.get_optimized_outs()
     
-######################################################
-# SURROGATE MODEL APPROXIMATOR FUNCS
-######################################################
+# ######################################################
+# # SURROGATE MODEL APPROXIMATOR FUNCS
+# ######################################################
 
-    def fit_model(self, x, y):
-        # call out to parent class to use surrogate model
-        self.sm_approx.fit(x,y)
+#     def fit_model(self, x, y):
+#         # call out to parent class to use surrogate model
+#         self.sm_approx.fit(x,y)
         
 
-    def model_predict(self, x, output_size=None):
-        # call out to parent class to use surrogate model
-        #'mean' is regressive definition. not statistical
-        #'variance' only applies for some surrogate models
+#     def model_predict(self, x, output_size=None):
+#         # call out to parent class to use surrogate model
+#         #'mean' is regressive definition. not statistical
+#         #'variance' only applies for some surrogate models
 
-        if output_size == None:
-            output_size = self.out_vars
+#         if output_size == None:
+#             output_size = self.out_vars
 
-        mean, noErrors = self.sm_approx.predict(x, output_size)
-        return mean, noErrors
+#         mean, noErrors = self.sm_approx.predict(x, output_size)
+#         return mean, noErrors
 
 
-    def model_get_variance(self):
-        variance = self.sm_approx.calculate_variance()
-        return variance
+#     def model_get_variance(self):
+#         variance = self.sm_approx.calculate_variance()
+#         return variance
 
 
 ######################################################
