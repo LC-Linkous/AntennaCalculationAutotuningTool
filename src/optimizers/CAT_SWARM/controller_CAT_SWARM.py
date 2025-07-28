eed1654210e0f27b7d4668dea6c8ed33ad898e38
LC_Linkous <L.C.Linkous@gmail.com>
2025-07-27 21:53:11 -0400

    get export config pass thru added

--- src/optimizers/CAT_SWARM/controller_CAT_SWARM.py
+++ src/optimizers/CAT_SWARM/controller_CAT_SWARM.py
@@ -42,6 +42,10 @@ class CONTROLLER_CAT_SWARM():
     def debug_message_printout(self, t):
         self.parent.updateStatusText(t)
 
+    def get_export_configuration(self):
+        # returns dictionary to parent class
+        return self.optimizer.export_configuration()
+    
 ####################################################
 # Toggle allow updates
 ####################################################
