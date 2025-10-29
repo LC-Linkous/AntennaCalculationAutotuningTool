03020bd98a8ae511451de51ecffd44dd5361956c
LC_Linkous <L.C.Linkous@gmail.com>
2025-10-29 16:47:53 -0400

    removed old debug comment

--- src/gui/page_project/page_project.py
+++ src/gui/page_project/page_project.py
@@ -76,7 +76,6 @@ class ProjectPage(wx.Panel):
 
 
     def applyLoadedProjectSettings(self, PC):
-        print("page_project applyLoadedProjectSettings()")
         self.notebook_project.applyLoadedProjectSettings(PC)
 
 
