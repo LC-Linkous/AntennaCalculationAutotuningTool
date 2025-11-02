ef6f17529e852af245062332987fa49944c7c51a
LC_Linkous <L.C.Linkous@gmail.com>
2025-11-02 15:07:54 -0500

    logging added (batch edit)

--- src/gui/page_optimizer/notebook_optimizer/optimizer_panels/glods_settings_panels/panel_multiglods.py
+++ src/gui/page_optimizer/notebook_optimizer/optimizer_panels/glods_settings_panels/panel_multiglods.py
@@ -4,7 +4,7 @@
 #   Class for inputs for multiGLODS optimizer
 #
 #   Author(s): Lauren Linkous (LINKOUSLC@vcu.edu)
-#   Last update: November 21, 2024
+#   Last update: November 2, 2025
 ##--------------------------------------------------------------------\
 
 
@@ -13,6 +13,8 @@ import wx
 import os
 import numpy as np
 import pandas as pd
+import logging
+logger = logging.getLogger(__name__)
 
 import project.config.antennaCAT_config as c
 
@@ -113,8 +115,8 @@ class MultiGLODS_Panel(wx.Panel):
             self.toleranceVal = self.fieldTolerance.GetValue()
             self.maxIter = self.fieldMaxIter.GetValue()
         except Exception as e:
-            print("ERROR in panel_multiglods getting user input")
-            print(e)
+            logger.error("ERROR in panel_multiglods getting user input")
+            logger.error(e)
             noError = False   
         
         return noError
