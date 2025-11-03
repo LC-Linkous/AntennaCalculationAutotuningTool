910975481b11a3902203ad520c598e6f864389e6
LC_Linkous <L.C.Linkous@gmail.com>
2025-11-02 20:24:24 -0500

    logging added (by optimizer)

--- src/optimizers/CHICKEN_SWARM_2015/chicken_swarm_python/chicken_swarm.py
+++ src/optimizers/CHICKEN_SWARM_2015/chicken_swarm_python/chicken_swarm.py
@@ -6,13 +6,15 @@
 #   A basic chicken swarm optimization class. 
 #
 #   Author(s): Lauren Linkous, Jonathan Lundquist
-#   Last update: May 18, 2025
+#   Last update: November 2, 2025
 ##--------------------------------------------------------------------\
 
 
 import numpy as np
 from numpy.random import Generator, MT19937
 import sys
+import logging
+logger = logging.getLogger(__name__)
 np.seterr(all='raise')
 
 
@@ -339,9 +341,8 @@ class swarm:
                 if iter > last_iter:
                     last_iter = iter
 
-            print("************************************************")
-            print("Internal Objective Function Iterations: " + str (iter))
-            print("Internal Best Eval: " + str(best_eval))
+            logger.info(f"Internal Objective Function Iterations: {iter}")
+            logger.info(f"Internal Best Eval: {best_eval}")
 
             # check if G_best of surrogate optimizer is better than what the main optimizer is finding
             potential_Gb =  np.array(surrogateOptimizer.get_optimized_soln()).reshape(1 ,-1)
@@ -349,16 +350,12 @@ class swarm:
             if np.linalg.norm(potential_F_Gb) < np.linalg.norm(self.F_Gb):
                 self.F_Gb = np.array(potential_F_Gb)
                 self.Gb = np.array(potential_Gb[0])
-                print("NEW BEST!!!!!!")      
-                print("self.F_Gb")
-                print(self.F_Gb)
-                print("self.Gb")
-                print(self.Gb)
+
             
             canUseSurrogate = True
             
         except Exception as e:
-            print(e)
+            logger.error(e)
             self.debug_message_printout("ERROR: failed to set up and minimize surrogate model")
 
         return canUseSurrogate
@@ -792,7 +789,7 @@ class swarm:
             self.current_particle = self.current_particle + 1
             if self.current_particle == self.number_of_particles:
                 if self.useSurrogateModel == True:
-                    print("MINIMIZING SURROGATE MODEL")
+                    logger.info("MINIMIZING SURROGATE MODEL")
                     self.minimize_surrogate_model()
                 self.current_particle = 0
 
@@ -925,7 +922,8 @@ class swarm:
 
     def debug_message_printout(self, msg):
         if self.parent == None:
-            print(msg)
+            pass
+            #print(msg)
         else:
             self.parent.debug_message_printout(msg)
 
