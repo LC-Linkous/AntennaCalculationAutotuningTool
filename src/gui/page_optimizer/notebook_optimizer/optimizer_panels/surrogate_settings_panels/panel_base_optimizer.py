090ca0f57926001e6eaba7d774a9ef9bf4e05326
LC_Linkous <L.C.Linkous@gmail.com>
2025-11-02 20:22:44 -0500

    swap to f string format

--- src/gui/page_optimizer/notebook_optimizer/optimizer_panels/surrogate_settings_panels/panel_base_optimizer.py
+++ src/gui/page_optimizer/notebook_optimizer/optimizer_panels/surrogate_settings_panels/panel_base_optimizer.py
@@ -192,7 +192,7 @@ class BaseOptimizerPage(wx.Panel):
             optimizerName = OPT_CHICKEN_QUANTUM
 
         else:
-            logger.error("ERROR in panel_surrogate.py unknown optimizer selected")
+            logger.error("ERROR in panel_base_optimizer.py unknown optimizer selected")
 
         return optimizerName
     
@@ -221,7 +221,7 @@ class BaseOptimizerPage(wx.Panel):
             df, noError = self.chicken_quantum_panel.getOptimizerInputs(is_surrogate)
         else:
             logger.error("ERROR: optimizer name not recognized in panel_base_optimizer. Select an option from the dropdown menu to continue!")
-            logger.error("selected optimizer: {optimizerName}")
+            logger.error(f"selected optimizer: {optimizerName}")
 
         if noError == False:
             logger.error("ERROR: error in optimizer input values. check inputs.")    
